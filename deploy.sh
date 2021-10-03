7b550e429fd705fd0e1a990548f37bc58ba9fb3b
Katharine Cordes <gillis.katharine@gmail.com>
2021-10-03 13:57:39 -0600

    hotfix: update production deploy to include migrations

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,8 @@ fi
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.ssl.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.ssl.yml up -d --remove-orphans
 
+# Update database
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.ssl.yml exec app php bin/console doctrine:migrations:migrate --no-interaction
+
 # Clean up old networks and images
 docker system prune -f
